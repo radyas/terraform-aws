da3caade8330df63e0c7f4bc76705b1de346a063
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2021-11-25 01:32:00 +0530

    script fixes

--- remote/script.sh
+++ remote/script.sh
@@ -3,9 +3,7 @@ ssh -T -o StrictHostKeyChecking=no -i private.pem ec2-user@"$1" << 'EOL'
     sudo yum update
     sudo yum install -y git
     git clone -b dev https://github.com/radyas/terraform-aws.git
-    aws configure set aws_access_key_id "$2"
-    aws configure set aws_secret_access_key "$3"
-    aws configure set default.region us-west-2
+    aws configure << "$2\$3"
     cd terraform-aws/api
     pip3 install -r requirements.txt
     bash
